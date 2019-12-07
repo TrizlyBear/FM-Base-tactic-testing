bc352c3e1955e620d166af74e8a7887f70c25538
TrizlyBear <38221328+TrizlyBear@users.noreply.github.com>
2019-12-07 23:34:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-axios.get('https://raw.githubusercontent.com/OvroExtraTime/tactictesting/master/tacticresults.json').then(res => {
-    var table = document.getElementById('tactictable').getElementsByTagName('tbody')[0];
-    tacticdata = res.data
-    res.data.sort(function(a, b){return b["Total Score"]-a["Total Score"]})
-    res.data.forEach(element => {
-        var newRow = table.insertRow(table.rows.length);
-        if(element["Total Score"] > 20) {
-            irow("#2ecc71") 
-        } else if(element["Total Score"] > 15) {
-            irow("#27AE60")
-        } else if(element["Total Score"] > 10){
-            irow("#f1c40f")
-        } else {
-            irow("#e74c3c")
-        }
-        function irow(color){newRow.innerHTML = `<td> ${element.Author} </td><td> ${element.Tactic} </td><td> ${element.Formation} </td> <td> ${element["Points Per Game"]} </td> <td> ${element["Win %"]} </td> <td> ${element["Draw %"]} </td> <td> ${element["Loss %"]} </td> <td> ${element["Goals For"]} </td> <td> ${element["Goals Against"]} </td> <td style="background:${color}"> ${element["Total Score"]} </td>`;  }
-    });
-})
+var tactics=[]
+var table;axios.get('https://raw.githubusercontent.com/OvroExtraTime/tactictesting/master/tacticresults.json').then(res=>{table=document.getElementById('tactictable').getElementsByTagName('tbody')[0];tacticdata=res.data
+tactics=res.data
+res.data.sort(function(a,b){return b["Total Score"]-a["Total Score"]})
+res.data.forEach(element=>{var newRow=table.insertRow(table.rows.length);console.log(element.URL)
+if(element["Total Score"]>20){irow("#2ecc71")}else if(element["Total Score"]>15){irow("#27AE60")}else if(element["Total Score"]>10){irow("#f1c40f")}else{irow("#e74c3c")}
+function irow(color){newRow.innerHTML=`<td> ${element.Author} </td><td> <a href="${element.URL}">${element.Tactic} </a></td><td> ${element.Formation} </td> <td> ${element["Points Per Game"]} </td> <td> ${element["Win %"]} </td> <td> ${element["Draw %"]} </td> <td> ${element["Loss %"]} </td> <td> ${element["Goals For"]} </td> <td> ${element["Goals Against"]} </td> <td style="background:${color}"> ${element["Total Score"]} </td>`}})})
+var cfilter='none';function filtersubtop(){var Parent=document.getElementById('tablebody');console.log(Parent.children.length)
+while(Parent.children.length>0&&(cfilter=='none'||cfilter=='underdog')){Parent.removeChild(Parent.lastChild);if(Parent.children.length==0){cfilter='subtop'
+tactics.forEach(element=>{var newRow=table.insertRow(table.rows.length);console.log(element.URL)
+if(element["Total Score"]>20){}else if(element["Total Score"]>15){}else if(element["Total Score"]>10){irow("#f1c40f")}else{}
+function irow(color){newRow.innerHTML=`<td> ${element.Author} </td><td> <a href="${element.URL}">${element.Tactic} </a></td><td> ${element.Formation} </td> <td> ${element["Points Per Game"]} </td> <td> ${element["Win %"]} </td> <td> ${element["Draw %"]} </td> <td> ${element["Loss %"]} </td> <td> ${element["Goals For"]} </td> <td> ${element["Goals Against"]} </td> <td style="background:${color}"> ${element["Total Score"]} </td>`}})}}}
+function filterunderdog(){var Parent=document.getElementById('tablebody');console.log(Parent.children.length)
+while(Parent.children.length>0&&(cfilter=='none'||cfilter=='subtop')){Parent.removeChild(Parent.lastChild);if(Parent.children.length==0){cfilter='underdog'
+tactics.forEach(element=>{var newRow=table.insertRow(table.rows.length);console.log(element.URL)
+if(element["Total Score"]>20){}else if(element["Total Score"]>15){}else if(element["Total Score"]>10){}else{irow("#e74c3c")}
+function irow(color){newRow.innerHTML=`<td> ${element.Author} </td><td> <a href="${element.URL}">${element.Tactic} </a></td><td> ${element.Formation} </td> <td> ${element["Points Per Game"]} </td> <td> ${element["Win %"]} </td> <td> ${element["Draw %"]} </td> <td> ${element["Loss %"]} </td> <td> ${element["Goals For"]} </td> <td> ${element["Goals Against"]} </td> <td style="background:${color}"> ${element["Total Score"]} </td>`}})}}}
+function filternone(){var Parent=document.getElementById('tablebody');console.log(Parent.children.length)
+while(Parent.children.length>0&&(cfilter=='underdog'||cfilter=='subtop')){Parent.removeChild(Parent.lastChild);if(Parent.children.length==0){cfilter='none'
+tactics.forEach(element=>{var newRow=table.insertRow(table.rows.length);console.log(element.URL)
+if(element["Total Score"]>20){irow("#2ecc71")}else if(element["Total Score"]>15){irow("#27AE60")}else if(element["Total Score"]>10){irow("#f1c40f")}else{irow("#e74c3c")}
+function irow(color){newRow.innerHTML=`<td> ${element.Author} </td><td> <a href="${element.URL}">${element.Tactic} </a></td><td> ${element.Formation} </td> <td> ${element["Points Per Game"]} </td> <td> ${element["Win %"]} </td> <td> ${element["Draw %"]} </td> <td> ${element["Loss %"]} </td> <td> ${element["Goals For"]} </td> <td> ${element["Goals Against"]} </td> <td style="background:${color}"> ${element["Total Score"]} </td>`}})}}}
