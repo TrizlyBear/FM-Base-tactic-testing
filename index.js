4f50a17b05e2bbc3fa1db08b41a9bb1082874023
TrizlyBear <38221328+TrizlyBear@users.noreply.github.com>
2019-12-07 23:46:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function irow(color){newRow.innerHTML=`<td> ${element.Author} </td><td> <a href=
 var cfilter='none';function filtersubtop(){var Parent=document.getElementById('tablebody');console.log(Parent.children.length)
 while(Parent.children.length>0&&(cfilter=='none'||cfilter=='underdog')){Parent.removeChild(Parent.lastChild);if(Parent.children.length==0){cfilter='subtop'
 tactics.forEach(element=>{var newRow=table.insertRow(table.rows.length);console.log(element.URL)
-if(element["Total Score"]>20){}else if(element["Total Score"]>15){}else if(element["Total Score"]>10){irow("#f1c40f")}else{}
+if(element["Total Score"]>10&&element["Total Score"]<16){irow("#f1c40f")}
 function irow(color){newRow.innerHTML=`<td> ${element.Author} </td><td> <a href="${element.URL}">${element.Tactic} </a></td><td> ${element.Formation} </td> <td> ${element["Points Per Game"]} </td> <td> ${element["Win %"]} </td> <td> ${element["Draw %"]} </td> <td> ${element["Loss %"]} </td> <td> ${element["Goals For"]} </td> <td> ${element["Goals Against"]} </td> <td style="background:${color}"> ${element["Total Score"]} </td>`}})}}}
 function filterunderdog(){var Parent=document.getElementById('tablebody');console.log(Parent.children.length)
 while(Parent.children.length>0&&(cfilter=='none'||cfilter=='subtop')){Parent.removeChild(Parent.lastChild);if(Parent.children.length==0){cfilter='underdog'
 tactics.forEach(element=>{var newRow=table.insertRow(table.rows.length);console.log(element.URL)
-if(element["Total Score"]>20){}else if(element["Total Score"]>15){}else if(element["Total Score"]>10){}else{irow("#e74c3c")}
+if(element["Total Score"]<11){irow("#e74c3c")}
 function irow(color){newRow.innerHTML=`<td> ${element.Author} </td><td> <a href="${element.URL}">${element.Tactic} </a></td><td> ${element.Formation} </td> <td> ${element["Points Per Game"]} </td> <td> ${element["Win %"]} </td> <td> ${element["Draw %"]} </td> <td> ${element["Loss %"]} </td> <td> ${element["Goals For"]} </td> <td> ${element["Goals Against"]} </td> <td style="background:${color}"> ${element["Total Score"]} </td>`}})}}}
 function filternone(){var Parent=document.getElementById('tablebody');console.log(Parent.children.length)
 while(Parent.children.length>0&&(cfilter=='underdog'||cfilter=='subtop')){Parent.removeChild(Parent.lastChild);if(Parent.children.length==0){cfilter='none'
